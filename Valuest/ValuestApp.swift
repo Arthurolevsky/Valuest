70d9cb955b548ee19f3731bd25863dc7556f56fd
Arthurolevsky <arthurolevsky@gmail.com>
2022-03-08 21:33:11 +0300

    Initial Commit

---
+++ Valuest/ValuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValuestApp.swift
+//  Valuest
+//
+//  Created by Arthur Olevsky on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ValuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
